55789aa856dfc21210003b5365cbf4f85239abe8
John Wells <molekewl2017@gmail.com>
2019-11-16 23:35:46 -0800

    validate user

--- auth/validate-user.js
+++ auth/validate-user.js
@@ -6,14 +6,14 @@ module.exports = (user, path) => {
   }
 
   if (!user.password || user.password.length < 4) {
-    errors.push("A password must be 8 or more characters long.");
+    errors.push("A password must be 4 or more characters long.");
   }
 
-  // if (path === "/register") {
-  //   if (!user.departments) {
-  //     errors.push("You must provide a department for the user.");
-  //   }
-  // }
+  if (path === "/register") {
+    if (!user.departments) {
+      errors.push("You must provide a department for the user.");
+    }
+  }
 
   return {
     isSuccessful: !Boolean(errors.length),
